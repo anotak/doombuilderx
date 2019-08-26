2a96d941b6e8a5e1cdc9fc7a700dd2252ee7a26f
anotak <anotak@users.noreply.github.com>
2019-08-26 18:29:15 -0500

    lua update

--- data/Lua/Examples/hello.lua
+++ data/Lua/Examples/hello.lua
@@ -11,4 +11,7 @@ UI.LogLine("There are several example scripts in the Lua subfolder in your Doom 
 UI.LogLine("You can get at those by using the Scripts submenu in the Lua menu")
 UI.LogLine("Or you can browse for them manually.")
 
+UI.LogLine("")
+UI.LogLine("For more information on the Lua API for writing your own scripts, check https://github.com/anotak/doombuilderx/wiki")
+
 UI.DebugLogLine("This line only shows up if an error or a warning occurs, otherwise it's invisible.")
